01562e786a3281fe773aeec4352e31cfcf6bed8b
8121409001 <113225311+8121409001@users.noreply.github.com>
2022-09-19 21:39:37 +0530

    Add files via upload

---
+++ student rollno.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class student
+{
+	public:
+		void getdata()
+		{
+		
+		int rollno,mark1,mark2;
+	    }
+};
+class student
+{
+	cout<<"enter the rollno:";
+	cin>>rollno;
+}
+class test:student
+{
+	cout<<"enter the mark1:"<<m1<<endl;
+	cout<<"enter the mark2:"<<m2<<endl;
+}
+class results:test
+{
+	cout<<"totalmark:";
+	totalmark=m1+m2;
+};
+int main()
+{
+	student getdata1();
+	student getdata();
+	return 0;
+}
