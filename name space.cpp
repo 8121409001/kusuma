9329dd42e8915e2c98fc670bc264866321580c94
8121409001 <113225311+8121409001@users.noreply.github.com>
2022-09-19 21:38:13 +0530

    Add files via upload

---
+++ name space.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+std::cout<<"input a:"; cin >>a;
+std::cout<<"input b:"; cin >>b;
+std::cout<<"sum is"<<a+b<<b;
+std::cout<<"product is"<<a*b<<b;
+return 0;
+}
