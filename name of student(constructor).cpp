0f17fdb27dab62eee5f7a4b0094284d7c81d5e09
8121409001 <113225311+8121409001@users.noreply.github.com>
2022-09-19 21:37:33 +0530

    Add files via upload

---
+++ name of student(constructor).cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class programming
+{
+	string x="cpp";
+	public:
+	 	programming()
+		programming(string);
+		void display();
+		
+};
+void main()
+{
+	program p;
+	p.display();
+	
+}
+programming::display()
+{
+	cout<<"i love programming";
+}
+programming::programming(string y)
+{
+	cout<<"i love"+y;
+}
+
