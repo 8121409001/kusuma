b5eefd9d0de61e7a145664eeb9e37ce60ef5c1c2
8121409001 <113225311+8121409001@users.noreply.github.com>
2022-10-14 21:56:13 +0530

    Add files via upload

---
+++ area of shapes.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int area(int a,int b)
+{
+	int area;
+	area=0.5*a*b;
+	cout<<"area of triangle"<<area<<endl;
+}
+int area(float r)
+{
+	int area;
+	area=3.14*r*r;
+	cout<<"area of circle"<<area<<endl;
+}
+int area(float a,float b)
+{
+	int area;
+	area=a*b;
+	cout<<"area of rectangle"<<area<<endl;
+}
+int area(int a)
+{
+	int area;
+	area=a*a;
+	cout<<"area of square"<<area<<endl;
+}
+int main()
+{
+	int x=2,y=3;
+	area(x,y);
+	area(2.6f);
+	area(3.2f,4.2f);
+	area(y);
+	return 0;
+}
