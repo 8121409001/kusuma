654197031076036ba6ec52703eb8721d85e2d0f5
8121409001 <113225311+8121409001@users.noreply.github.com>
2022-10-14 18:12:32 +0530

    Add files via upload

---
+++ string conversion from lower case to upper case.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cctype>
+using namespace std;
+int main()
+{
+	char a[]={"hello"};
+	cout<<"input is"<<a;
+	for(int x=0;x<strlen[a];x++)
+	{
+		putchar(toupper(a[x]));
+	}
+	return 0;
+}
