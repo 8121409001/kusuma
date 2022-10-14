2b74e06e2b25b7a11a6fcdf55befd53cf9c48f04
8121409001 <113225311+8121409001@users.noreply.github.com>
2022-10-14 23:10:48 +0530

    Add files via upload

---
+++ average marks of a student.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+class student
+{
+	char name[20];
+	int m1,m2,m3,m4,avg;
+	public:
+		void input();
+		void display();
+		void calculation();
+};
+void student::input()
+{
+	cout<<"enter the student name=";
+	cin>>name;
+	cout<<"mark1";
+	cin>>m1;
+	cout<<"mark2";
+	cin>>m2;
+	cout<<"mark3";
+	cin>>m3;
+	cout<<"mark4";
+	cin>>m4;
+}
+void student::calculation()
+{
+	cout<<"total marks="<<m1<<m2<<m3<<m4<<endl;
+	avg=(m1+m2+m3+m4),4;
+	cout<<"average marks="<<avg<<endl;
+}
+void student::display(){
+	{
+		if(avg>75)
+		{
+			cout<<"grade=S";
+		}
+		else if(avg>60 and avg<75)
+		{
+			cout<<"grade=A";
+		}
+		else if(avg>50 and avg<60)
+		{
+			cout<<"grade=B";
+		}
+		else if(avg<50)
+		{
+			cout<<"grade=fail";
+		}
+		else
+		{
+			cout<<"enter valid input";
+		}
+	}
+	int main()
+	{
+		Student s;
+		s.input();
+		s.calculation();
+		s.display();
+		return 0;
+	}
+
